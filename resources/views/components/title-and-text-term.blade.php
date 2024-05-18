a6d46841bb9e5c7727646a5860f6c633ae6357de
Professor <daniel.rsouza@ulbra.br>
2024-05-18 17:54:33 -0300

    ajuste na exibicao dos termos

--- resources/views/components/title-and-text-term.blade.php
+++ resources/views/components/title-and-text-term.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     <h3 class="text-base leading-6 font-semibold mb-4" style="color: rgba(75, 85, 99, 1)">
-        $title
+        {{$title}}
     </h3>
-    <p class="font-normal text-base leading-6">$term</p>
+    <p class="font-normal text-base leading-6 mb-8" style="color: rgba(75, 85, 99, 1)">{{$term}}</p>
 </div>
