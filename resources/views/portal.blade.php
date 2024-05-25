65a2096062ee40cffa29ebc3581085f5beebd1af
Juliano Ramos Matos <julianomatos.si@gmail.com>
2024-05-25 11:01:55 -0300

    Update portal.blade.php
    Ajuste do Título e ícone da página

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ajuda Tchê</title>
+
+        <link rel="shortcut icon" href="{{ asset('imagens/rs2.jpg') }}" type="image/x-icon">
 
             <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
